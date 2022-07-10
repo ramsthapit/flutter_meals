0adc63191102fca1ee93185ca6393116099547a6
Ram Sthapit <ramsthapit50@gmail.com>
2022-07-10 18:10:57 +0545

    passing Data via Constructor

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -2,42 +2,41 @@ import 'package:flutter/material.dart';
 import 'package:meals_app/category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
+  final String id;
   final String title;
   final Color color;
 
-  CategoryItem(this.title, this.color);
+  CategoryItem(this.id, this.title, this.color);
 
-  void selectCategory(BuildContext ctx){
+  void selectCategory(BuildContext ctx) {
     Navigator.of(ctx).push(MaterialPageRoute(builder: (_) {
-      return const CategoryMealsScreen();
+      return CategoryMealsScreen(id, title);
     }));
   }
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: (() => selectCategory(context)),
-      splashColor: Theme.of(context).primaryColor,
-      borderRadius: BorderRadius.circular(15),
-
-      child: Container(
-        padding: EdgeInsets.all(15),
-        child: Text(
-          title,
-          style: Theme.of(context).textTheme.headline6,
-        ),
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              color.withOpacity(0.7),
-              color,
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
+        onTap: (() => selectCategory(context)),
+        splashColor: Theme.of(context).primaryColor,
+        borderRadius: BorderRadius.circular(15),
+        child: Container(
+          padding: EdgeInsets.all(15),
+          child: Text(
+            title,
+            style: Theme.of(context).textTheme.headline6,
           ),
-          borderRadius: BorderRadius.circular(15),
-        ),
-      )
-    );
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                color.withOpacity(0.7),
+                color,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            borderRadius: BorderRadius.circular(15),
+          ),
+        ));
   }
 }
--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
-  const CategoryMealsScreen({Key? key}) : super(key: key);
+  final String categoryId;
+  final String categoryTitle;
+  CategoryMealsScreen(this.categoryId, this.categoryTitle);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('The Recepies'),),
-      body: const Center(
-        child: Text('The Recepies for the Categpry!'),
-      )
-    ); 
+        appBar: AppBar(
+          title: Text(categoryTitle),
+        ),
+        body: const Center(
+          child: Text('The Recepies for the Categpry!'),
+        )
+      );
   }
 }
